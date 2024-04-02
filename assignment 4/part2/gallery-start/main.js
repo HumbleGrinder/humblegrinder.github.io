3bed42ad993ef9680e611b7b8acb054634c4248b
HumbleGrinder <ammar.sajid16@gmail.com>
2024-04-01 20:54:42 -0400

    Update main.js

--- assignment 4/part2/gallery-start/main.js
+++ assignment 4/part2/gallery-start/main.js
@@ -6,16 +6,28 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
-const alts = {
-  'pic1.jpg' : 'Human eye',
-  'pic2.jpg' : 'Rock',
-  'pic3.jpg' : 'Flowers',
-  'pic4.jpg' : 'Hieroglyphics',
-  'pic5.jpg' : 'Butterfly'
-}
+
 /* Declaring the alternative text for each image file */
+const alts = {
+    'pic1.jpg' : 'Human eye',
+    'pic2.jpg' : 'Rock',
+    'pic3.jpg' : 'Flowers',
+    'pic4.jpg' : 'Hieroglyphics',
+    'pic5.jpg' : 'Butterfly'
+}
 
 /* Looping through images */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/${image}');
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', x => {
+        displayedImage.src = x.target.src;
+        displayedImage.alt = x.target.src;
+    });
+}
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
