c0b345d3e91a8714e90ee502c9854ab5dbecb5ba
HumbleGrinder <ammar.sajid16@gmail.com>
2024-04-01 21:08:54 -0400

    added alt text and final

--- assignment 4/part2/gallery-start/main.js
+++ assignment 4/part2/gallery-start/main.js
@@ -5,10 +5,20 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Looping through images */
-for (let k = 1; k <= 5; k++){
+
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const alts = {
+  'pic1.jpg' : 'Eye',
+  'pic2.jpg' : 'Rock',
+  'pic3.jpg' : 'Flowers',
+  'pic4.jpg' : 'Hieroglyphics',
+  'pic5.jpg' : 'Butterfly'
+};
+
+for (let i = 0; i < images.length; i++){
   const newImage = document.createElement('img');
-  newImage.setAttribute('src', 'images/pic' + k + '.jpg');
+  newImage.setAttribute('src', 'images/' + images[i]);
+  newImage.setAttribute('alt', alts[images[i]]); // Set alt text using the alts object
   thumbBar.appendChild(newImage);
 
   newImage.addEventListener('click', setImage);
@@ -16,8 +26,9 @@ for (let k = 1; k <= 5; k++){
 
 function setImage(e){
   const imgPath = e.target.getAttribute('src');
+  const imgAlt = e.target.getAttribute('alt'); // Get alt text
 
   // set thumbnail as the picture displayed 
   displayedImage.setAttribute('src', imgPath);
+  displayedImage.setAttribute('alt', imgAlt); // Set alt text
 }
-
