76f44c1fa8a7d28db6c6287b0d5c89a873ff7bd9
HumbleGrinder <ammar.sajid16@gmail.com>
2024-04-01 20:48:10 -0400

    upadated image filenames

--- assignment 4/part2/gallery-start/main.js
+++ assignment 4/part2/gallery-start/main.js
@@ -5,7 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : 'Human eye',
+  'pic2.jpg' : 'Rock',
+  'pic3.jpg' : 'Flowers',
+  'pic4.jpg' : 'Hieroglyphics',
+  'pic5.jpg' : 'Butterfly'
+}
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
